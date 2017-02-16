fb528db9aa3a14d948a58d739bf3a2775ffd358a
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-02-17 04:25:59 +0500

    working on contentIntent

--- app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
+++ app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -1,11 +1,17 @@
 package com.example.android.background.utilities;
 
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 
+import com.example.android.background.MainActivity;
 import com.example.android.background.R;
 
 /**
@@ -31,9 +37,24 @@ public class NotificationUtils {
                 .setContentIntent(contentIntent(context))
                 .setAutoCancel(true);
 
-        
+        if (Build.VERSION.SDK_INT >=  Build.VERSION_CODES.JELLY_BEAN){
+            notificationBulider.setPriority(Notification.PRIORITY_HIGH);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(WATER_REMINDER_NOTIFICATION_ID,notificationBulider.build());
 
 
+    }
+
+    private static PendingIntent contentIntent(Context context){
+
+        Intent startActivityIntent = new Intent(context, MainActivity.class);
+
+        return PendingIntent.getActivities(
+                context,
+                )
     }
 
 }
